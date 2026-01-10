2c48c1fdd965ac23d6f05f7a65656337d821b344
Kiran Kadam <kirankkadam@yahoo.com>
2026-01-10 23:49:40 +1100

    Implemented the GET person method

--- WorkBench/Controllers/PersonController.cs
+++ WorkBench/Controllers/PersonController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkBench.DB;
 
 namespace WorkBench.Controllers
 {
@@ -6,10 +8,33 @@ namespace WorkBench.Controllers
     [Route("Person")]
     public class PersonController : ControllerBase
     {
+        private readonly WorkBenchDbContext _workBenchDbContext;
+        public PersonController(WorkBenchDbContext workBenchDbContext)
+        {
+            _workBenchDbContext = workBenchDbContext;
+        }
+
         [HttpGet("GetAllPeople")]
         public async Task<IActionResult> GetAllPeople()
         {
-            return Ok();
+            var people = _workBenchDbContext.Persons?.ToListAsync();
+            if(people is null)
+            {
+                return NotFound();
+            }
+
+            var result = new List<ViewModels.Person>();
+            foreach (var person in await people)
+            {
+                var personViewModel = new ViewModels.Person
+                {
+                    Id = person.Id,
+                    FullName = person.FullName
+                };
+
+                result.Add(personViewModel);
+            }
+            return Ok(result);
         }
     }
 }
