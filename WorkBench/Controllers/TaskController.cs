1b38f7962f2de1233be4e679dcb1c7a672f8938f
Kiran Kadam <kirankkadam@yahoo.com>
2026-01-11 00:41:41 +1100

    Implemented the GET method for TaskController

--- WorkBench/Controllers/TaskController.cs
+++ WorkBench/Controllers/TaskController.cs
@@ -1,16 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WorkBench.DB;
 
 namespace WorkBench.Controllers
 {
     [ApiController]
     [Route("Task")]
     public class TaskController : ControllerBase
-
     {
+        private WorkBenchDbContext _workBenchDbContext;
+        public TaskController(WorkBenchDbContext workBenchDbContext)
+        {
+            _workBenchDbContext = workBenchDbContext;
+        }
+
         [HttpGet("GetAllTasks")]
         public async Task<IActionResult> GetAllTasks()
         {
-            return Ok();
+            var tasks = _workBenchDbContext.Tasks?.ToList();
+            if(tasks is null)
+            {
+                return NotFound();
+            }
+
+            var taskViewModels = tasks.Select(t => new ViewModels.Task
+            {
+                Id = t.Id,
+                Title = t.Title,
+                Description = t.Description
+            }).ToList();
+
+            return Ok(taskViewModels);
         }
     }
 }
