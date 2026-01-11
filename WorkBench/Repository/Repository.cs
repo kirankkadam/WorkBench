c93c7dd502721fe2ad28c02b53d6a59b4a8d8d04
Kiran Kadam <kirankkadam@yahoo.com>
2026-01-11 23:39:34 +1100

    Implemented IRepository pattern

---
+++ WorkBench/Repository/Repository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkBench.DB;
+using WorkBench.Repository.Interfaces;
+
+namespace WorkBench.Repository
+{
+    // Generic Repository Implementation
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly WorkBenchDbContext _context;
+        public Repository(WorkBenchDbContext context) => _context = context;
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+        public async Task<T?> GetByIdAsync(int id) => await _context.Set<T>().FindAsync(id);
+        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
+        public async Task UpdateAsync(T entity) => _context.Set<T>().Update(entity);
+        public async Task DeleteAsync(int id)=> _context.Set<T>().Remove(await GetByIdAsync(id) ?? throw new ArgumentException("Entity not found"));
+    }
+}
